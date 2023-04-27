37dda9b92e662fb368ea9aecd7f3636fe74b35f3
Ben Apprill <benapprill@gmail.com>
2023-04-26 23:02:01 -0500

    Added Vector2 Vector3 Out Reactions.

---
+++ Assets/Abstractions/_ReactionDefinitions/OutReactions/AVector2OutReaction.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public abstract class AVector2OutReaction : ScriptableObject
+{
+    public abstract Vector2 React();
+}
---
+++ Assets/Abstractions/_ReactionDefinitions/OutReactions/AVector3OutReaction.cs
@@ -0,0 +1,3 @@
+public abstract class AVector3OutReaction
+{
+}
